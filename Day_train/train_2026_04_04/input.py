0335a5e35cd197e41312da7610e9e2f25c5cde5e
raylin <raylin703@gmail.com>
2026-04-04 10:24:15 +0800

    no message

---
+++ Day_train/train_2026_04_04/input.py
@@ -0,0 +1,13 @@
+#input 使用者輸入
+
+# name = input("請輸入姓名:")
+# print(f"你的名字是 {name}")
+
+#練習一 : 矩形面積計算
+
+# lenght = float(input("請輸入矩形的長度:"))
+# widht =float(input("請輸入矩形的寬度:"))
+
+# rectangle = lenght * widht
+
+# print(f"矩形面積是{rectangle}")
